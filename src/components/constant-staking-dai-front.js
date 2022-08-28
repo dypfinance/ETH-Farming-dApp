e20c15f99464490a81b6ec9e9c74fb3e2f8c8385
doyledyp <doyledev@protonmail.com>
2022-08-28 21:32:47 +0300

    Change Text Expired Pool

--- src/components/constant-staking-dai-front.js
+++ src/components/constant-staking-dai-front.js
@@ -230,7 +230,8 @@ export default function initConstantStakingNew({ staking, apr, liquidity='ETH', 
             e.preventDefault()
 
             if (this.state.stakingTime != 0 && (Date.now() - this.state.stakingTime >= this.state.cliffTime)) {
-                window.$.alert('Contract Expired! Your lock time ended so please withdraw your funds and move to a new pool.')
+                window.$.alert('Contract Expired! Your lock time ended so please withdraw your funds and move to a ' +
+                    'new pool. Any unclaimed rewards will be automatically distributed to your wallet within 24 hours!')
                 return;
             }
 
